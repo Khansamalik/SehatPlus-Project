71bad2bb6b51757097c193101ff02414a83e719b
Khansamalik <kghafoor.bsbi24sines@student.nust.edu.pk>
2025-08-18 14:39:16 +0500

    Update API URLs to use VITE_API_URL

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,7 +1,7 @@
 // frontend/src/api/http.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const http = axios.create({ baseURL: API_URL });
 
